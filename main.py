ee04b4746b04196c5b19c3eaf3161b544cf001ef
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-05-21 19:08:24 +0300

    add Get number of players handler, gameplay in work

--- main.py
+++ main.py
@@ -27,6 +27,12 @@ db = SQLiteDB("game.db")
 db.connect()
 db.init_game_table()
 
+class GameStates(StatesGroup):
+    num_players = State()
+    num_rolls = State()
+    rolls = State()
+    winner = State()
+
 
 # Start command handler
 @dp.message_handler(Command("start"))
@@ -39,33 +45,72 @@ async def cmd_start(message: types.Message):
 async def cmd_play(message: types.Message, state: FSMContext):
     session_id = db.create_session()
     db.create_game(message.from_user.id, session_id)
-    await message.reply(Localization.number_rolls)
+    await message.reply(Localization.number_players)
+    await GameStates.num_players.set()
+
+# Get number of players handler
+@dp.message_handler(state=GameStates.num_players)
+async def handle_num_players(message: types.Message, state: FSMContext):
+    try:
+        message.text.isdigit()
+        num_players = int(message.text)
+        if num_players >= 1:
+            await state.update_data(num_players=num_players)
+            data = await state.get_data()
+            for i in range(num_players):
+                player_name = f"player {i+1}"
+                data[player_name] = []
+                await state.update_data(data = data)
+            await message.reply(Localization.number_rolls)
+            await GameStates.num_rolls.set()
+        else:
+            await bot.send_message(chat_id=message.chat.id, text=Localization.number_players)    
+    except:
+        await bot.send_message(chat_id=message.chat.id, text=Localization.number_players)
+
 
 # Get number of rolls handler
-@dp.message_handler(content_types=types.ContentType.TEXT)
+@dp.message_handler(state=GameStates.num_rolls)
 async def handle_message(message: types.Message, state: FSMContext):
     if message.text.isdigit():
         num_rolls = int(message.text)
         await state.update_data(num_rolls = num_rolls)
-        await state.update_data(rolls = [])
         await message.reply(Localization.lets_play, reply_markup=GameState())
+        await GameStates.rolls.set()
     else:
         await bot.send_message(chat_id=message.chat.id, text = Localization.number_rolls)
 
 
+
 #Roll dice handler
-@dp.callback_query_handler(lambda c: c.data == GameState.ROLL_DICE)
+@dp.callback_query_handler(lambda c: c.data == GameState.ROLL_DICE, state=GameStates.rolls)
 async def handle_button(callback_query: types.CallbackQuery, state: FSMContext):
     dice_roll = await bot.send_dice(chat_id=callback_query.message.chat.id)
     dice_roll_value = dice_roll["dice"]["value"]
     data = await state.get_data()
+    print(data)
     num_rolls = data["num_rolls"]
-    rolls = data["rolls"]
-    rolls.append(dice_roll_value)
-    result = f"Roll {len(rolls)}: {dice_roll_value}\n"
+    num_gamers = data["num_players"]
+    print((num_rolls))
+    print((num_gamers))
+    for i in range(num_gamers):
+        print(i)
+        await state.update_data(i = [])
+        num_player = i
+        rolls = data[num_player]
+        rolls.append(dice_roll_value)
+        result = f"Roll of player {num_player}: {dice_roll_value}\n"
     if len(rolls) == num_rolls:
         await asyncio.sleep(3)
-        result += "Total result: " + str(sum(rolls))
+        total_results = []
+        for i in range(data["num_players"]):
+            player_result = []
+            for j in range(num_rolls):
+                player_result.append(rolls[i*num_rolls+j])
+            total_results.append(sum(player_result))
+            result += f"Player {i+1}: {player_result}\n"
+        winner_index = total_results.index(max(total_results))
+        result += f"Winner: Player {winner_index+1}\nTotal result: {total_results}"
         await bot.send_message(chat_id=callback_query.message.chat.id, text=result)
         await state.finish()
     else:
--- resources/Localization.py
+++ resources/Localization.py
@@ -1,5 +1,6 @@
 class Localization:
     welcome = "Welcome to the dice game! Use the /play command to start the game."
     number_rolls = "Enter the number of throws"
+    number_players = "Enter the number of players"
     lets_play = "Let's play the dice game! Click the 'Roll Dice' button to roll the dice."
     rolled = "You rolled {}!"
