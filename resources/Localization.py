d4ac1375f1010f75e9a2e5e20571df05339643a2
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-05-23 18:42:39 +0300

    change description

--- resources/Localization.py
+++ resources/Localization.py
@@ -2,5 +2,5 @@ class Localization:
     welcome = "Welcome to the dice game! Use the /play command to start the game."
     number_players = "Welcome to the dice game! Please enter the number of players"
     number_rolls = "Please enter the number of throws"
-    lets_play = "Let's play the dice game! Click the 'Roll Dice' button to roll the dice."
+    lets_play = "Let's play a game of dice! Press the 'Roll the Dice' button to roll the dice. You have {} tries. At the end of the game, you will see the total of your rolls. After all players have rolled the dice, each player's score and the winner will be displayed."
     rolled = "You rolled {}!"
