825c3442d6e3476c2bfe77f48edf3706afa1f068
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-06-12 15:02:33 +0300

    upded func using new url

--- NFT/NftWorker.py
+++ NFT/NftWorker.py
@@ -2,27 +2,24 @@ import requests
 import json
 
 USER_ADDRESS = "EQDuVM_M2Cqx0bco0cYlNeW_aa2mvtvLy-UuPJQkzZEVmrmN"
+OWNER_ADDRESS = "0%3A89f356bd10b3c8609187c5abcd7bb1d5840c7f8a88e73debff8e64ffd8f12010"
 
 
 class NFTWorker:
-    BASE_URL = "https://tonapi.io/v2/nfts"
+    BASE_URL = "https://tonapi.io/v2"
 
     def get_data(self, user_address):
-        url = f"{self.BASE_URL}/collections/{user_address}/items?limit=1000&offset=0"
-        owner_address = (
-            "0:89f356bd10b3c8609187c5abcd7bb1d5840c7f8a88e73debff8e64ffd8f12010"
-        )
+        url = f"{self.BASE_URL}/accounts/{OWNER_ADDRESS}/nfts?collection={user_address}&limit=1000&offset=0&indirect_ownership=false"
+
         try:
             response = requests.get(url)
             data = response.json()["nft_items"]
-            for i in range(len(data)):
-                if data[i]["owner"]["address"] == owner_address:
-                    metadata = data[i]["metadata"]
-                    name, description, image_url = (
-                        metadata["name"],
-                        metadata["description"],
-                        metadata["image"],
-                    )
-                    return name, description, image_url
+            metadata = data[0]["metadata"]
+            name, description, image_url = (
+                metadata["name"],
+                metadata["description"],
+                metadata["image"],
+            )
+            return name, description, image_url
         except:
             print("Unsupported adress")
