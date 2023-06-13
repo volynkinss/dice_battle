0accceac8c37496ea4b4404575acfe742fe5d7e2
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-06-13 14:51:18 +0300

    edited get_nfts func to save data nfts as dict

--- NFT/NftWorker.py
+++ NFT/NftWorker.py
@@ -1,25 +1,29 @@
 import requests
-import json
 
 USER_ADDRESS = "EQDuVM_M2Cqx0bco0cYlNeW_aa2mvtvLy-UuPJQkzZEVmrmN"
-OWNER_ADDRESS = "0%3A89f356bd10b3c8609187c5abcd7bb1d5840c7f8a88e73debff8e64ffd8f12010"
+OWNER_ADDRESS = "0%3A69171dc426e7761ceca7eca4f3f9228ec1dc268a4199613f058e334408eab215"
 
 
 class NFTWorker:
     BASE_URL = "https://tonapi.io/v2"
 
-    def get_data(self, user_address):
+    def get_nfts(self, user_address):
         url = f"{self.BASE_URL}/accounts/{OWNER_ADDRESS}/nfts?collection={user_address}&limit=1000&offset=0&indirect_ownership=false"
 
         try:
             response = requests.get(url)
             data = response.json()["nft_items"]
-            metadata = data[0]["metadata"]
-            name, description, image_url = (
-                metadata["name"],
-                metadata["description"],
-                metadata["image"],
-            )
-            return name, description, image_url
+            data_nfts = {}
+            for i in range(len(data)):
+                metadata = data[i]["metadata"]
+                i += 1
+                id = i
+                name, description, image_url = (
+                    metadata["name"],
+                    metadata["description"],
+                    metadata["image"],
+                )
+                data_nfts[id] = (name, description, image_url)
+            return data_nfts
         except:
             print("Unsupported adress")
